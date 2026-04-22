d4a7162c63f6b571c9ddec0377379a4790004ebb
Marley <marleyjonatha138@gmail.com>
2026-04-21 22:59:15 -0300

    Super-Mario2

---
+++ cenas/player.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+
+class_name Player
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+enum PlayerMode{
+	SMALL,
+	BIG,
+	SHOOTING
+}
+
+@onready var animated_sprite_2d = $AnimatedSprite2D as PlayerAnimatedSprite
+@onready var area_collision_shape_2d = $Area2D/CollisionShape2D
+@onready var body_collision_shape_2d = $BodyCollisionShape2D
+@onready var area_2d = $Area2D
+
+@export_group("Locomotion")
+@export var run_speed_damping = 0.5
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export_group("")
+
+@export_group("Stomping Enemies")
+@export var min_stomp_degree = 35
+@export var max_stomp_degree = 145
+@export var stomp_y_velocity = -150
+@export_group("")
+
+var player_mode = PlayerMode.SMALL
+
+
+func _physics_process(delta):
+	
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.5
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = lerp(velocity.x, speed * direction, run_speed_damping * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		
+	animated_sprite_2d.trigger_animation(velocity, direction, player_mode)
+	
+	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if area is Enemy:
+		handle_enemy_collision(area)
+
+func handle_enemy_collision(enemy: Enemy):
+	if enemy == null:
+		return
+	
+	if is_instance_of(enemy, Koopa) and (enemy as Koopa).in_a_shell:
+		(enemy as Koopa).on_stomp(global_position)
+	else:
+		var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+		if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
+			enemy.die()
+			on_enemy_stomped()
+	
+func on_enemy_stomped():
+	velocity.y = stomp_y_velocity
